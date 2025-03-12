db45fe04b001db41181f931e1744b58f876b9f33
Chia113310202 <t113310202@ntut.org.tw>
2025-03-12 19:44:27 +0800

    Update Lab03_Q1.cpp

--- Lab03/Lab03_Q1.cpp
+++ Lab03/Lab03_Q1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-const int Max_Degree = 2; //最大的次方是200 
+const int Max_Degree = 200; //最大的次方是200 
 
 class Polynomial {
 private:
@@ -15,7 +15,7 @@ public:
         cout << "輸入最高次方: ";
         cin >> degree;
         cout << "從最高次方輸入係數: ";
-        for (int i = degree; i >= 0; --i) //因為是從最高次方開始，所以要--i 
+        for (int i = degree; i >= 0; i--) //因為是從最高次方開始，所以要i-- 
 		{
             cin >> coef[i];
         }
@@ -24,32 +24,41 @@ public:
     //輸出多項式
     void print() const 
 	{
-        bool first = true; //第一個先設true，這樣第一項前面就不會有+號 
-        for (int i = degree; i >= 0; --i) //從最高次方開始輸出 
+        int count = 0; //計算輸出幾項了 
+        for (int i = degree; i >= 0; i--) //從最高次方開始輸出 
 		{
             if (coef[i] != 0) //係數是0的就不會顯示
 			{ 
-                if (!first) cout << " + "; //不是第一項就加+號 
-                if (i == 0) //(i == 0)代表是常數項，所以輸出不用加 x^
-				{
-                    cout << coef[i]; 
-                }
-				else 
-				{
-                    cout << coef[i] << "x^" << i;
-                }
-                first = false; //第一項之後就改成 false 
+                if (count > 0) //因為第一項的 count還等於零，所以沒有輸出+號 
+					cout << " + "; 
+            }    
+            cout << coef[i]; 
+            
+            if (i > 0) //常數項不用加 x^
+			{ 
+                cout << "x^" << i;
             }
+
+            count++; //輸出一項就加上去 
         }
-        cout << endl;
-    }
+		cout << endl;
+	}
+    
+        
+    
 
     //加法
     Polynomial add(const Polynomial &p) const 
 	{
         Polynomial result;
-        result.degree = max(degree, p.degree); //新的多項式次方是兩個多項式中比較大的那一個 
-        for (int i = 0; i <= result.degree; ++i) 
+        //新的多項式次方是兩個多項式中比較大的那一個
+        if (degree > p.degree) {
+        	result.degree = degree;
+    	} else {
+        	result.degree = p.degree;
+    	}  
+        
+		for (int i = 0; i <= result.degree; i++) 
 		{
             result.coef[i] = coef[i] + p.coef[i]; //係數相加 
         }
@@ -61,9 +70,9 @@ public:
 	{
         Polynomial result;
         result.degree = degree + p.degree; //新的多項式次方是兩個多項式次方的和 
-        for (int i = 0; i <= degree; ++i) 
+        for (int i = 0; i <= degree; i++) 
 		{
-            for (int j = 0; j <= p.degree; ++j) 
+            for (int j = 0; j <= p.degree; j++) 
 			{
                 result.coef[i + j] += coef[i] * p.coef[j]; //係數相乘 
             }
@@ -75,29 +84,45 @@ public:
     float eval(float x) const 
 	{
         float result = 0;
-        for (int i = 0; i <= degree; ++i) 
+        for (int i = 0; i <= degree; i++) 
 		{
             result += coef[i] * pow(x, i); //係數乘以x的幾次方 
         }
         return result;
     }
     
-    //輸出結果 
-    void output(const Polynomial& p1, const Polynomial& p2, float x) const 
+    //加法
+    void printAdd(const Polynomial& p1, const Polynomial& p2) const 
 	{
-        //加法
-        Polynomial sum = p1.add(p2); //p1跟 p2帶回 add那邊算一算之後再傳回來 
+        Polynomial sum = p1.add(p2); //p1跟 p2帶回 add那邊算一算之後再傳回來
         cout << "Add: ";
-        sum.print(); //輸出 p1跟 p2相加的結果 
-
-        //乘法
+        sum.print(); //輸出 p1跟 p2相加的結果
+    }
+    //乘法
+    void printMult(const Polynomial& p1, const Polynomial& p2) const 
+	{
         Polynomial product = p1.mult(p2); //p1跟 p2帶回 mult那邊算一算之後再傳回來
         cout << "Mult: ";
         product.print(); //輸出 p1跟 p2相乘的結果 
+    }
+    //帶入x值 
+    void printEval(const Polynomial& p, float x) const 
+	{
+        cout << "Polynomial = p(" << x << ") = " << p.eval(x) << endl;
+    }
 
-        //帶入x值 
-        cout << "Polynomial 1 = p(" << x << ") = " << p1.eval(x) << endl;
-        cout << "Polynomial 2 = p(" << x << ") = " << p2.eval(x) << endl;
+	//輸出結果 
+    void output(const Polynomial& p1, const Polynomial& p2, float x) const 
+	{
+		//加法 
+		printAdd(p1, p2);
+
+        //乘法
+        printMult(p1, p2);
+
+        //帶 x的值
+        printEval(p1, x);
+        printEval(p2, x);
     }
 };
 
