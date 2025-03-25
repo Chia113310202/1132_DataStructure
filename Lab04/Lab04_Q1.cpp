5f8b001da968707491d3d9806a06ca5796cd0133
Chia113310202 <t113310202@ntut.org.tw>
2025-03-25 15:55:59 +0800

    Update Lab04_Q1.cpp

--- Lab04/Lab04_Q1.cpp
+++ Lab04/Lab04_Q1.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+using namespace std;
+// 表示單張撲克牌的類別
+class Card {
+public:
+    string colors; //儲存撲克牌的花色
+    string rank; //儲存撲克牌的數值
+    Card(string s, string r) : colors(s), rank(r) {} //建立constructor來初始化物件，當Card物件建立時，它會自動執行這個函式，並把s和r的值存入colors和rank
+    void display() const { //顯示撲克牌的資訊
+        cout << rank << " of " << colors << endl;
+    }
+};
+
+// 實作Stack
+class Stack {
+private:
+    vector<Card> stack; //表示stack是一個能存放Card類別物件的vector
+
+public:
+    void push(const Card& card) {
+    	cards.push_back(card)
+    }
+
+    Card pop() {
+    	if(!isEmpty)
+    	{
+    		cards.pop_back(card)
+		}
+    }
+
+    bool isEmpty() const {
+    	return cards.empty();
+    }
+};
+
+// 代表一副撲克牌的類別
+class Deck {
+private:
+    vector<Card> cards; // 存放未洗牌的撲克牌
+    Stack shuffledDeck; // 存放洗過的牌，用實作的stack來管理
+public:
+    Deck() { //建立constructor來初始化物件
+        string colors[] = { "Hearts", "Diamonds", "Clubs", "Spades" }; //儲存撲克牌的花色
+        string ranks[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" }; //儲存撲克牌的數值
+
+        //利用迴圈將52張牌加入cards這個vector裡面
+        for (int i = 0; i < 4; i++) { // 四種花色
+            for (int j = 0; j < 13; j++) { // 13 種點數
+                cards.push_back(Card(colors[i], ranks[j]));
+            }
+        }
+    }
+
+    //洗牌(Hint:使用shuffle函數)
+    void shuffleDeck() {
+        
+    }
+
+    //發牌
+    void drawAllCards() {
+
+    }
+};
+
+int main() {
+    Deck deck; //建立deck產生52張撲克牌
+    deck.shuffleDeck(); //進行洗牌並放入堆疊
+    cout << "Shuffled deck:" << endl;
+    deck.drawAllCards(); //依序取出堆疊內的牌並顯示
+    return 0;
+}
+
+
 
