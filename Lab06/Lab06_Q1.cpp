3acc2101a80539830aef1bf5e9569d2ddd1c5d90
Chia113310202 <t113310202@ntut.org.tw>
2025-04-08 23:06:05 +0800

    Update Lab06_Q1.cpp

--- Lab06/Lab06_Q1.cpp
+++ Lab06/Lab06_Q1.cpp
@@ -19,41 +19,36 @@ public:
     // Push 操作：將元素放入堆疊
     void push(char ch) {
     	Node* newNode = new Node(); // 建立新節點
-        newNode->data = ch;      // 儲存資料
+        newNode->data = ch;         // 儲存資料，把字元放進節點 
         newNode->next = top;        // 新節點指向原本的頂端
         top = newNode;              // 更新頂端為新節點
-        //cout << ch << " 已推入堆疊\n";
     }
 
     // Pop 操作：移除並回傳頂端元素
     char pop() {
-    	if (top == NULL) {  // 如果堆疊是空的
+    	if (top == nullptr) {  // 如果堆疊是空的
             cout << "堆疊為空，無法取出元素。\n";
-            return;            // 結束這次 pop()
+            return '\0';            // 結束這次 pop()
         }
 
         int poppedValue = top->data; // 取得頂端元素的值
         Node* temp = top;            // 用 temp 暫存目前頂端節點
         top = top->next;             // 更新頂端為下一個節點
         delete temp;                 // 釋放記憶體，刪除原頂端節點
-        //cout << "已取出元素: " << poppedValue << endl;
- 
-    }
+		return poppedValue;          // 回傳剛剛的資料 
+    }  
 
     // Peek 操作：取得頂端元素但不移除
     char peek() {
-    	int peekValue = top->data; // 取得頂端元素的值
-        Node* temp = top;            // 用 temp 暫存目前頂端節點
-        top = top->next;             // 更新頂端為下一個節點
-       
+    	if (top == nullptr) {
+            return '\0';
+        }
+        return top->data;
     }
-
+    
     // 判斷堆疊是否為空
     bool isEmpty() {
-    	if (top->next == NULL){
-    		return true;
-		}
-      	return false;
+    	return top == nullptr;
     }
 };
 
@@ -67,11 +62,46 @@ int precedence(char op) {
 
 // 將中序表達式 (infix) 轉換為後序表達式 (postfix)
 void InfixToPostfix(const char* infix, char* postfix) {
-	string postfix;
-	char c = '+' || '-' || '*' || '/';
-	if (infix == char c);
-		postfix = postfix + c;
-	if (infix == '(')
+	Stack stack; //建一個 stack來放運算子 
+    int j = 0; //j是 postfix的寫入位置 
+
+    for (int i = 0; i < strlen(infix); i++) {
+        char c = infix[i];
+        
+		// 如果是英文或數字，就直接加入到 postfix 
+        if (isalnum(c)) {
+            postfix[j++] = c;
+        }
+        // 如果是 (，就直接放到 stack
+        else if (c == '(') {
+            stack.push(c);
+        }
+        // 如果是 (，要把括號中的運算子都拿出來
+        else if (c == ')') {
+            while (!stack.isEmpty()) {
+    			char top = stack.pop();
+    			if (top == '(') { // 遇到 ( 就停 
+        			break;  
+    			}
+    			postfix[j++] = top;  // 其他的加進 postfix
+  			}
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/') {
+        	// stack頂端的運算值優先度比較高，就先拿出來 
+            while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                postfix[j++] = stack.pop();
+            }
+            stack.push(c); // 最後再把自己放入 stack 
+        }
+    }
+
+    // 清空堆疊剩下的東西
+    while (!stack.isEmpty()) {
+    	// 把堆疊裡剩下的運算子都拿出來加到 postfix
+        postfix[j++] = stack.pop();
+    }
+
+    postfix[j] = '\0'; // 結尾
 		
 
 }
@@ -86,5 +116,3 @@ int main() {
 
     return 0;
 }
-
-
